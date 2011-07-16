d597e5521bcac194a2fc207e3b716698b115e9f9
Brad Wilson <brad@lucky-dip.net>
2011-07-16 17:38:56 +1000

    Initial commit to filtered_db_dump.

---
+++ test/test_filtered_db_dump.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestFilteredDbDump < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
